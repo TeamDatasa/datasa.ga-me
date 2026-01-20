21b5d75321a1a01060b20cee123aaf6f084f506c
jiwon-88 <jiwong821@gmail.com>
2026-01-20 12:48:29 +0900

    feat: entity 수정
    -trip테이블 theme 컬럼 추가

--- src/main/java/datasa/controller/TripController.java
+++ src/main/java/datasa/controller/TripController.java
@@ -1,16 +1,17 @@
 package datasa.controller;
 
+import datasa.entity.Trip;
 import datasa.service.TripService;
 import domain.dto.TripListResponse;
 import domain.dto.TripWriteRequest;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
--- src/main/java/datasa/service/TripService.java
+++ src/main/java/datasa/service/TripService.java
@@ -8,6 +8,8 @@ import domain.dto.TripListResponse;
 import domain.dto.TripWriteRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
