30fc6c96f303d70bb38571fa117c9e85a9cf1fff
wncidnr <wncidnr@gmail.com>
2026-01-21 09:39:46 +0900

    feat: 회원가입 로직 구현 및 validation, security 설정

--- src/main/java/datasa/config/SecurityConfig.java
+++ src/main/java/datasa/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package datasa.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class SecurityConfig {
 	@Bean
