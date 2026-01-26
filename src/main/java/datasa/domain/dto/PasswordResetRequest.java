4c3a495c9757cfcb3eb5e4b6573507ede38046f3
wncidnr <wncidnr@gmail.com>
2026-01-26 12:44:27 +0900

    feat 비밀번호 재설정 로직 구현 중

---
+++ src/main/java/datasa/domain/dto/PasswordResetRequest.java
@@ -0,0 +1,12 @@
+package datasa.domain.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PasswordResetRequest {
+	@Email @NotBlank
+	private String email;
+}
